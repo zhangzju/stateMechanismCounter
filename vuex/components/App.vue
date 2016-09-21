a43279abe1eab2b0219fb8272c737c80364a158f
zhangzju <zhangway19921221@163.com>
2016-09-22 02:16:36 +0800

    Initial commit

---
+++ vuex/components/App.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div>
+    <Display></Display>
+    <Increment></Increment>
+  </div>
+</template>
+
+<script>
+import Dsiplay from './Display.vue'
+import Increment from './Increment.vue'
+import store from '../vuex/store'
+
+export default {
+  data() {
+    return {
+    };
+  },
+  computed: {},
+  ready() {},
+  attached() {},
+  methods: {},
+  components: {
+    Display:Display,
+    Increment: Increment
+  },
+  store:store
+};
+</script>
+
+<style lang="css">
+</style>
---
+++ vuex/components/Increment.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="increment">
+    <button>Increase</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  computed: {},
+  ready() {},
+  attached() {},
+  methods: {},
+  components: {}
+};
+</script>
+
+<style lang="css">
+</style>
